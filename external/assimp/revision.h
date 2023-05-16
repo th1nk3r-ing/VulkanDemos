751124f1f34829018d82d52bbd7e3201b9811d88
think3r <think3r@foxmail.com>
2023-05-16 16:59:05 +0800

    chore: AS 最新版本编译 OK

--- external/assimp/revision.h
+++ external/assimp/revision.h
@@ -1,7 +1,7 @@
 #ifndef ASSIMP_REVISION_H_INC
 #define ASSIMP_REVISION_H_INC
 
-#define GitVersion 0x0612725d
+#define GitVersion 0x78abfef1
 #define GitBranch "master"
 
 #endif // ASSIMP_REVISION_H_INC
